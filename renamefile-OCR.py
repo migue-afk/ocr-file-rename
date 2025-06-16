6776f568f47755f6abddb8e015ad62a2271f3668
abig4m <miguef10hd@gmail.com>
2025-06-15 19:32:15 -0500

    add date_extract_dateparser

--- renamefile-OCR.py
+++ renamefile-OCR.py
@@ -34,4 +34,29 @@ def clean_ocr_text(text):
     text = re.sub(r'\s+', ' ', text).strip()
     return text
 
-def process_name(name)
+def extarct_date(raw2)
+
+    date_pattern = re.findall(  r'(\d{1,2}\s+de\s+\w+\s+\d{4}|\d{1,2}[/-]\d{1,2}[/-]\d{2,4}|\d{4}-\d{2}-\d{2})',raw2,flags = re.IGNORECASE)
+
+    if not date_pattern:
+        raw2 = 'datenotfound'
+        return raw2
+    
+    for date_str in date_pattern:
+        date_obj = dateparser.parse(date_str)
+        if date_obj:
+            raw2 = date.obj.strftime('%d-%m-%Y') #date format ISO
+            break   # Show only the first result
+        else:
+            raw2 = 'datenotfound'
+    return raw2
+ 
+if __name__ == '__main__':
+
+    for pdf_file in natsorted(inputpdf.glob("*.pdf")):
+        raw1 = pdftotext(pdf_file)
+        if not raw1:
+            continue
+    clean_text = clean_ocr_text(raw1) 
+    date_text = extract_date(clean_text)
+
