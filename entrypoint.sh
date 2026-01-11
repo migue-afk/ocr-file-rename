5c24ebb6b4c73b996ae16c1c4460cec7ddb12811
migue-afk <58987458+migue-afk@users.noreply.github.com>
2026-01-11 09:57:51 -0500

    Refactor entrypoint.sh to simplify execution logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 
-echo "Init Proyect"
+# Init cron in backgroung
+#cron
 
-if [ -f "$1" ]; then
-	chmod +x "$1"
-	name=$(echo "$1" | awk -F. '{print $2}')
-	echo -e "$name"
-	if [[ $name == "sh" ]]; then
-		#Execute .sh
-		./"$1"
-	else
-		#Execute .py
-		echo "soy .py"
-		python "$1"
-#	./"$1"
-	fi
-else
-	echo "Path '$1' does not exist"
-	exit 1
-fi
+# Ejec the principal script
+/ocrapp/sep.sh
+
+#tail -f /var/log/cron.log
